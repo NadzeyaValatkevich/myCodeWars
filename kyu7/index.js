9d82c90001a94a3feb17d235d3296e7ec49e02aa
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2022-07-16 13:41:41 +0300

    added task: Number of Divisions

--- kyu7/index.js
+++ kyu7/index.js
@@ -14,4 +14,18 @@ var FindFunction = function(func, arr) {
    
   };
   
-  console.log(FindFunction([(a=>a%2==0),9,3,1,0],[1,2,3,4]))
+console.log(FindFunction([(a=>a%2==0),9,3,1,0],[1,2,3,4]))
+
+//task2 Number of Divisions
+const divisions = (n, divisor) => {
+  let num = 0
+  if(n >= divisor) {
+      n = n / divisor;
+      num += 1;
+   return num + divisions(n, divisor);
+  } else {
+      return 0
+  }
+};
+
+console.log(divisions(100, 2))
